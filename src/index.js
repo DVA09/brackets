161c5baa3e586ca98eb0743259bc43e8cd13fcc8
Vladimir Deushev <1991rand@gmail.com>
2023-10-18 19:27:21 +0300

    feat: finished brackets task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ module.exports = function check(str, bracketsConfig) {
   const counting = Math.round(str.length / 2);
   const brackets = [];
   let newStr = str;
-  
+  for (let item of bracketsConfig) {
+    brackets.push(item.join(''));
+  }
+  for (let i = 0; i <= counting; i++) {
+    if (newStr.length > 1 && newStr.length !== 0) {
+      for (let bracket of brackets) {
+        newStr = newStr.replaceAll(bracket, '');
+      }
+    }
+  }
+  if (newStr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }  
 }
